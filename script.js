9349535b0e02102f60a8240068e4c11405df35e8
Gabriel <gabrielcsilvasg@gmail.com>
2022-01-14 16:55:59 -0300

    Capturar elementos HTML

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem =document.querySelector('img');
+const botao = document.querySelector('button');
+const nomePersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const estado = document.querySelector('#estado');
