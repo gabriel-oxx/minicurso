4aae35a12d3a77b605a641d7f3a3ab6e5f7851ad
Gabriel <gabrielcsilvasg@gmail.com>
2022-01-15 12:52:37 -0300

    adicionar segundo personagem

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const botao = document.querySelector('button');
 const nomePersonagem1 = document.querySelector('#nome');
 const especiePersonagem1 = document.querySelector('#especie');
 const estadoPersonagem1 = document.querySelector('#estado');
+const nomePersonagem2 = document.querySelector('#nnome');
+const estadoPersonagem2 = document.querySelector('#eestado');
+const especiePersonagem2 = document.querySelector('#eespecie');
 
 verificarEstado = (data) =>{
 	if(data.status == 'unknown'){
@@ -34,5 +37,22 @@ pegarPersonagemUm = () =>{
 		nomePersonagem1.innerHTML = data.name;
 		especiePersonagem1.innerHTML = data.species;
 		estadoPersonagem1.innerHTML = verificarEstado(data);
+	});
+}
+
+pegarPersonagemDois = () =>{
+	let numeroAleatorio = gerarValorAleatorio();
+	return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			"Content-type": 'application/json'
+		}
+	}).then((response)=>response.json()).then((data)=>{
+		imagem.src = data.image;
+		imagem.alt = data.name;
+		nomePersonagem2.innerHTML = data.name;
+		especiePersonagem2.innerHTML = data.species;
+		estadoPersonagem2.innerHTML = verificarEstado(data);
 	});
 }
